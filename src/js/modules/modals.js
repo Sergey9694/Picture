4905b8122f4f03b1ae8824ac34067740906df756
Sergei <drobilenko94@mail.ru>
2022-10-19 16:26:38 +0300

    added animation of modal windows

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -24,6 +24,7 @@ const modals = () => {
 
                 windows.forEach(item => {
                     item.style.display = 'none';
+                    item.classList.add('animated', 'fadeIn');
                 });
     
                 modal.style.display = 'block';
