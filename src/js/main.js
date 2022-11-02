5da289119ae64e9cd1c318ea0a9d8ee711fe5a75
Sergei <drobilenko94@mail.ru>
2022-11-02 17:22:17 +0300

    added functionality to change the block on hover

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureResize from "./modules/pictureResize";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -22,4 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price', formState);
     filter();
+    pictureResize('.sizes-block');
 });
